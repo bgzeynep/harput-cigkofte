1fda3bc4e762055fa9b8553b061b693deb344665
bgzeynep <zarici1834@gmail.com>
2026-08-18 00:06:58 +0300

    sitemap eklendi

---
+++ app/sitemap.js
@@ -0,0 +1,16 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://harputcigkofte.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://harputcigkofte.com/menu",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://harputcigkofte.com/iletisim",
+      lastModified: new Date(),
+    },
+  ];
+}
